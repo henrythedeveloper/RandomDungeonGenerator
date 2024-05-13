3fb57f917e1311877182f6042d59a5a5006f75d4
Diego Alas <diego.alas@ucdenver.edu>
2024-05-13 12:45:54 -0600

    Created Stats.cpp and Stats.h. Finished setters,  getters and constructors

---
+++ Stats.cpp
@@ -0,0 +1,18 @@
+/*
+Stats.cpp
+Diego
+Contains all the functions in stats.h
+*/
+#include "Stats.h"
+
+
+
+Stats::Stats() {}
+
+Stats::Stats(int strength, int intelligence, int speed, int maxHealth, int maxMana):
+    strength{strength}, intelligence{intelligence}, speed{speed}, maxHealth{maxHealth}, maxMana{maxMana}
+{
+
+}
+
+
---
+++ Stats.h
@@ -0,0 +1,59 @@
+/*
+Stats.h
+Diego
+A class that cointains all the stats for each entity
+5/13/24
+*/
+#ifndef STATS_H
+#define STATS_H
+
+
+class Stats
+{
+
+protected:
+int strength = 1;
+int intelligence = 1;
+int speed = 1;
+int maxHealth = 10;
+int maxMana = 10;
+
+
+
+public:
+Stats();
+Stats(int st, int in, int sp, int maxHP, int maxMA);
+
+//getters
+int getSt() {return strength;}
+int getIn() {return intelligence;}
+int getSp() {return speed;}
+int getMaxHP() {return maxHealth;}
+int getMaxMA() {return maxMana;}
+
+//setters
+int setSt(int newSt) {strength = newSt;}
+int setIn(int newIn) {intelligence = newIn;}
+int setSp(int newSp) {speed = newSp;}
+int setHp(int newMH) {maxHealth = newMH;}
+int setMp(int newMM) {maxMana = newMM;}
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
